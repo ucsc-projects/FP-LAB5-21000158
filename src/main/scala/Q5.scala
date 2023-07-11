6ddc4520577f9057b75000528372d26f9b4b3351
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-07-11 10:14:25 +0530

    Modify Qestion5

--- src/main/scala/Q5.scala
+++ src/main/scala/Q5.scala
@@ -4,10 +4,10 @@ object Q5 {
   def main(args: Array[String]): Unit = {
     print("Please input an integer: ")
     val inp = StdIn.readInt()
-    println(addOddNumbers(inp))
+    println(addEvenNumbers(inp))
   }
 
-  def isOdd(a: Int): Boolean = {
+  def isEven(a: Int): Boolean = {
     if (a % 2 == 0) {
       false
     } else {
@@ -15,15 +15,15 @@ object Q5 {
     }
   }
 
-  def addOddNumbers(a: Int): Int = {
+  def addEvenNumbers(a: Int): Int = {
     if (a <= 1) {
-      return 1
+      return 0
     }
     var sum: Int = 0
-    if (isOdd(a)) {
+    if (!isEven(a)) {
       sum = sum + a
     }
-    sum + addOddNumbers(a - 1)
+    sum + addEvenNumbers(a - 1)
   }
 
 }
