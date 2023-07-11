e28e98bd259e28e537497afaedfbbc14b0b55329
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-07-11 09:38:01 +0530

    Finish Question4

--- src/main/scala/Q4.scala
+++ src/main/scala/Q4.scala
@@ -1,3 +1,17 @@
-object Q4 {
+import scala.io.StdIn
 
+object Q4 {
+  def main(args: Array[String]): Unit = {
+    print("Please input an integer: ")
+    val inp = StdIn.readInt()
+    oddOrEven(inp)
+  }
+
+  def oddOrEven(a:Int){
+    if (a%2==0){
+      println("Even")
+    }else{
+      println("Odd")
+    }
+  }
 }
